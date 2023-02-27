172d3792fa8f7468cd492279217ba1ad9ad3411f
Foo Suon Chuang <foosuonchuang@linux6.cs.uchicago.edu>
2023-02-27 15:59:08 -0600

    included concatenation func

--- clean_census.py
+++ clean_census.py
@@ -35,3 +35,21 @@ def clean_census(input_filename, output_filename):
     output_file = df.to_csv(output_filename, index = False)
 
     return output_file
+
+def combine_dataframes_by_state(main_df, df_lst):
+    """
+    Recursively concatenates multiple panda dataframes (with "State" 
+    as the index) with only the required columns
+
+    Inputs:
+        df_lst (lst of tuples): (df, [cols to extract])
+        
+    Returns:
+        final_df (pandas series): concatenated pandas dataframes
+    """
+    if len(df_lst) == 0:
+        return main_df
+    
+    other_df, col_lst = df_lst.pop()
+    new_df = main_df.merge(other_df[col_lst], on="State")
+    return combine_dataframes_by_state(new_df, df_lst)
