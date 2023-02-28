24cc5a3351d4bc95cbf4f43cfdff013d212ad05e
Yujie Jiang <yujie0706@linux6.cs.uchicago.edu>
2023-02-28 01:51:08 -0600

    2016

--- scrape_funds.py
+++ scrape_funds.py
@@ -90,54 +90,68 @@ for year in [2016]:
                 writer.writerow(row_vals)
 
 # disparse the loop into one year: 2016
+
 total_data = pd.read_csv("state_total_data.csv")
-url = "https://api.usaspending.gov"
-endpoint = "/api/v2/search/spending_by_category/naics"
-dict = {}
+list_states = total_data["code"]
 
-for state in total_data["code"]:
+def data_year(list_states):
+        
+    url = "https://api.usaspending.gov"
+    endpoint = "/api/v2/search/spending_by_category/naics"
+    dict = {}
 
-    payload = {
-        "filters": {
-        "recipient_location": [
-            {
-                "country": "USA",
-                "state": f"{state}",
-                "county": "",
-                "city": "",
-                "zip": "",
-                "congressional_district": ""
+    for state in list_states:
+
+        payload = {
+            "filters": {
+            "recipient_location": [
+                {
+                    "country": "USA",
+                    "state": state,
+                    "county": "",
+                    "city": "",
+                    "zip": "",
+                    "congressional_district": ""
+                }
+            ],
+            "time_period": [
+                {"start_date": "2016-01-01",
+                "end_date": "2016-12-31"}]
             }
-        ],
-        "time_period": [
-            {"start_date": "2016-01-01",
-            "end_date": "2016-12-31"}]
-        }
-        ,
-        "page": 1,
-        "limit": 100,
-        "category": "naics"
-        }
+            ,
+            "page": 1,
+            "limit": 100,
+            "category": "naics"
+            }
 
-    results = []
-    response = requests.post(f"{url}{endpoint}", json = payload)
-    data = response.json()["results"]
-
-    while len(data) == 100:
+        results = []
         response = requests.post(f"{url}{endpoint}", json = payload)
         data = response.json()["results"]
-        results.extend(data)
-        if len(data) < 100:
-            break
-        payload["page"] += 1
 
+        while len(data) == 100:
+            response = requests.post(f"{url}{endpoint}", json = payload)
+            data = response.json()["results"]
+            results.extend(data)
+            if len(data) < 100:
+                break
+            payload["page"] += 1
+        
+        dict[state] = results
+                
+    with open("2016.csv", "w", newline = "") as csvfile:
 
+        writer = csv.writer(csvfile)
+        headers = list(dict[state][0].keys())
+        headers.append('State')
+        writer.writerow(headers)
 
+        for state, results in dict.items():
+            for row in results:
+                row_vals = list(row.values())
+                row_vals.append(state)
+                writer.writerow(row_vals)
 
-
-
-
-
+    return 
 
 
 # try
