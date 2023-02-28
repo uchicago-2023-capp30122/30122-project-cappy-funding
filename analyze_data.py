9ca9c4f15c3e1b7440e84d82b9387e6e62702aed
Foo Suon Chuang <foosuonchuang@linux6.cs.uchicago.edu>
2023-02-28 17:08:17 -0600

    updated w global var

--- analyze_data.py
+++ analyze_data.py
@@ -1,7 +1,7 @@
 import pandas as pd
 from clean_census import clean_census_expenditure, clean_census_population, clean_census_poverty
 from clean_funding import clean_funding
-from utils_clean_and_analyze import combine_dataframes_by_state
+from utils_clean_and_analyze import combine_dataframes_by_state, STATE_NAMES, STATE_NAMES_AND_UNITED_STATES, US_STATE_CODES, NAICS_SECTOR_CODES
 
 YEARS = ["2016", "2017", "2018", "2019", "2020"]
 
@@ -32,8 +32,8 @@ def clean_and_combine(years):
         expenditure_csv = year + expenditure_file_name # "2016_us_state_finances.csv"
         funding_csv = year + funding_file_name # "2016_us_funding.csv"
 
-        expenditure_df = pd.read_csv(expenditure_csv)
-        funding_df = pd.read_csv(funding_csv)
+        expenditure_df = clean_census_expenditure(pd.read_csv(expenditure_csv))
+        funding_df = clean_funding(pd.read_csv(funding_csv))
 
         if year == "2020":
             cleaned_and_combined[year] = combine_dataframes_by_state(expenditure_df, [(funding_df, []), (pop_df, []), (poverty_df, [])])
