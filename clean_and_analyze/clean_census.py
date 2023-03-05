b577739f4d5f5d2abb0dc1b7d7550c6545f6fe84
Foo Suon Chuang <foosuonchuang@linux6.cs.uchicago.edu>
2023-03-04 22:03:45 -0600

    included commetns and docstrings

--- clean_and_analyze/clean_census.py
+++ clean_and_analyze/clean_census.py
@@ -1,29 +1,33 @@
 import pandas as pd
 from .utils_clean_and_analyze import STATE_NAMES_AND_UNITED_STATES
 
-
 def clean_census_expenditure(df):
     """
-    ###
+    Cleans and analyzes a raw file of the government expenditure for one year
+
+    Inputs:
+        df (a pandas series): government expenditure by state
+    
+    Returns:
+        df (a pandas series): a cleaned and analyzed expenditure dataframe
     """
 
     df['Description'] = df['Description'].str.strip()
-
     df.rename(columns = {'United States Total':'United States'}, inplace = True)
 
-    # Retains only columns with combined state and local government expenditure
+    # Retains only columns of state and local combined government expenditure
     df = df[["Description"] + STATE_NAMES_AND_UNITED_STATES]
 
     # Drops rows relating to state revenue sources
     df.drop(df.index[0:66], inplace=True)
 
+    # Retains only specific expenditure categories
     social = ["Public welfare", "Hospitals", "Health", "Employment security administration", "Veterans' services"]
     educ = ["Education", "Libraries"]
     govt = ["Financial administration", "Judicial and legal", "General public buildings", "Other governmental administration"]
     transport = ["Highways", "Air transportation (airports)", "Parking facilities", "Sea and inland port facilities"]
     others = ["Utility expenditure", "Expenditure1"]
 
-    # Retains only required rows
     df = df[df["Description"].isin(social + educ + govt + transport + others)]
 
     # Transposes dataframe and sets new column names
@@ -40,7 +44,7 @@ def clean_census_expenditure(df):
             df[col] = df[col].str.replace(',','')
             df[col] = df[col].astype(int)
     
-    # Merges sub-categories into single categories
+    # Sums individual expenditure categories into four main categories
     df["Health and Social Services Expenditure"] = df[social].sum(axis=1)
     df["Education Related Expenditure"] = df[educ].sum(axis=1)
     df["Public Administration Expenditure"] = df[educ].sum(axis=1)
@@ -76,14 +80,27 @@ def clean_census_expenditure(df):
 
 def clean_census_population(pop_df):
     """
-    ###
+    Cleans and analyzes a raw file of the country's population size
+
+    Inputs:
+        pop_df (a pandas series): population size by state
+    
+    Returns:
+        pop_df (a pandas series): a cleaned and analyzed population dataframe
     """
+
+    # Retains only the first two columns of the dataframe
     pop_df = pop_df.iloc[:,0:2]
     pop_df.columns = ["State", "Population"]
     pop_df['State'] = pop_df['State'].str.strip()
+
+    # Retains only rows for states and country total (drops territories)
     pop_df = pop_df[pop_df["State"].isin(STATE_NAMES_AND_UNITED_STATES)]
+
+    # Converts row values into integers
     pop_df["Population"] = pop_df["Population"].str.replace(',','')
     pop_df["Population"] = pop_df["Population"].astype(int)
+
     pop_df.set_index("State", inplace=True)
 
     return pop_df
@@ -91,12 +108,22 @@ def clean_census_population(pop_df):
 
 def clean_census_poverty(poverty_df):
     """
-    ###
+    Cleans and analyzes a raw file of the country's poverty rate
+
+    Inputs:
+        poverty_df (a pandas series): poverty rate by state
+    
+    Returns:
+        poverty_df (a pandas series): a cleaned and analyzed poverty rate dataframe
     """
+    # Retains only the first two columns of the dataframe
     poverty_df = poverty_df.iloc[:,0:2]
     poverty_df.columns = ["State", "3-Year Average Poverty Rate (2018-2020)"]
     poverty_df['State'] = poverty_df['State'].str.strip()
+
+    # Retains only rows for states and country total (drops territories)
     poverty_df = poverty_df[poverty_df["State"].isin(STATE_NAMES_AND_UNITED_STATES)]
+
     poverty_df.set_index("State", inplace=True)
 
     return poverty_df
