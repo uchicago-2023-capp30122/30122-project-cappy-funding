21b4ad7047c77a9ef7a0df2fe6fd3e57581ec81f
Yujie Jiang <yujie0706@linux6.cs.uchicago.edu>
2023-03-06 10:29:40 -0600

    n

--- fund_data/scrape_funds.py
+++ fund_data/scrape_funds.py
@@ -41,59 +41,54 @@ def data_year(list_states):
     url = "https://api.usaspending.gov"
     endpoint = "/api/v2/search/spending_by_category/naics"
     dict = {}
+    for year in ['2016', '2017', '2018', '2019', '2020']
+        for state in list_states:
 
-    for state in list_states:
+            payload = {
+                "filters": {
+                "place_of_performance_locations": [
+                {"country": "USA",
+                "state": state}
+                
+                ],
+    
+                "time_period": [
+                    {"start_date": f"{year}-01-01",
+                    "end_date": f"{year}-12-31"}],
+                "lower_bound": 0
+                }
+                ,
+                "page": 1,
+                "limit": 100,
+                "category": "naics"
+                }
 
-        payload = {
-            "filters": {
-            "place_of_performance_locations": [
-            {"country": "USA",
-             "state": state}
-            
-            ],
- 
-            "time_period": [
-                {"start_date": "2018-01-01",
-                "end_date": "2018-12-31"}],
-            "lower_bound": 0
-            }
-            ,
-            "page": 1,
-            "limit": 100,
-            "category": "naics"
-            }
-
-        results = []
-        response = requests.post(f"{url}{endpoint}", json = payload)
-        data = response.json()["results"]
-
-        while len(data) == 100:
+            results = []
             response = requests.post(f"{url}{endpoint}", json = payload)
             data = response.json()["results"]
-            results.extend(data)
-            if len(data) < 100:
-                break
-            payload["page"] += 1
-        
-        dict[state] = results
-                
-    with open("data/raw_data/2016_us_funding.csv", "w", newline = "") as csvfile:
 
-        writer = csv.writer(csvfile)
-        headers = list(dict[state][0].keys())
-        headers.append('State')
-        writer.writerow(headers)
+            while len(data) == 100:
+                response = requests.post(f"{url}{endpoint}", json = payload)
+                data = response.json()["results"]
+                results.extend(data)
+                if len(data) < 100:
+                    break
+                payload["page"] += 1
+            
+            dict[state] = results
+                    
+        with open(f"data/raw_data/{year}_us_funding.csv", "w", newline = "") as csvfile:
 
-        for state, results in dict.items():
-            for row in results:
-                row_vals = list(row.values())
-                row_vals.append(state)
-                writer.writerow(row_vals)
+            writer = csv.writer(csvfile)
+            headers = list(dict[state][0].keys())
+            headers.append('State')
+            writer.writerow(headers)
+
+            for state, results in dict.items():
+                for row in results:
+                    row_vals = list(row.values())
+                    row_vals.append(state)
+                    writer.writerow(row_vals)
 
     return 
-
-data_2016 = pd.read_csv("2016.csv")
-data_2016 = data_2016.drop(columns = ['id'], axis = 1)
-data_2016.to_csv("2016_data.csv", index = False)
-
 
