57d0df61e2b432cc659c1fbe493914922ea1345c
Foo Suon Chuang <foosuonchuang@linux6.cs.uchicago.edu>
2023-03-06 15:49:09 -0600

    updated interface

--- cappy_funding/app.py
+++ cappy_funding/app.py
@@ -1,10 +1,7 @@
 import sys
-import pathlib
 from cappy_funding.data.raw_data import scrape_funds
 from cappy_funding.clean_and_analyze import analyze_data
 import warnings
-
-warnings.filterwarnings("ignore", category=DeprecationWarning)
 
 YEARS = ["2016", "2017", "2018", "2019", "2020"]
 CLEAN_DATA_DIR = "./cappy_funding/data/clean_data/"
